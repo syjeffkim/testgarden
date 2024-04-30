40cb4cbcaaf9a13c521146a1a8c2ac978d2159a9
Nicholas Angelopoulos <leaf@Nicholass-MacBook-Pro.local>
2024-04-30 17:25:45 -0400

    eslint configuration completed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,84 @@
+// Linting and Code Analysis: Linters like ESLint use this information to understand which variables are global in a 
+// given environment and should not be flagged as undefined. This helps in configuring the linter to avoid false 
+// warnings about undefined variables that are actually globals provided by the environment.
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+// react uses different styling so eslint needs to account for that
+import js from '@eslint/js';
+import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
+const { nodeBuiltin: node, browser } = globals;
+import react from 'eslint-plugin-react';
+// import jest from 'eslint-plugin-jest';
 
+// so eslint doesn't effect our tests files put ontop of test files or files to be ignored by eslint
+/* eslint-disable */
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReactConfig,
+  js.configs.recommended, // j: preset of recommended ESLint rules
+  {
+    rules: {
+      'require-atomic-updates': 'error', 
+      'arrow-body-style': ['error', 'as-needed'], // enforces no braces where they can be omitted (for arrow functions)
+      eqeqeq: 'error',
+      'no-console': 'warn',
+      'prefer-const': 'error',
+    },
+  },
+  // {     
+  //   files: ["tests/**/*"],
+  //   plugins: {jest, },
+  //   env: {
+  //     "jest/globals": true
+  //   },  
+  // },
+  {
+    files: ['server/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...node,
+      },
+    },
+  },
+  {
+    files: ['client/**/*.{js,jsx}'],
+    ...reactRecommended,
+    plugins: {
+      react,
+    },
+
+    languageOptions: { // configures eslint to handle jsx syntax which is used in react
+      ...reactRecommended.languageOptions,
+      parserOptions: { // configures eslint to handle jsx syntax which is used in react
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...browser,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    files: ['client/**/*.jsx'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    },
+  },
+  {
+    ignores: ['node_modules', 'client/dist/'],
+  },  
 ];
+
+
+
+
+// export default [
+//   {languageOptions: { globals: globals.browser }},
+//   pluginJs.configs.recommended,
+//   pluginReactConfig,
+// ];
